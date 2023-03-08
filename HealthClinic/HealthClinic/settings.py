10188f3c9dfc590899d56fa0d082d6a40af7ae69
Nemanja Cvejić <37965912+nemanja023@users.noreply.github.com>
2023-03-08 20:02:00 +0100

    modified files for starting on local

--- HealthClinic/HealthClinic/settings.py
+++ HealthClinic/HealthClinic/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = django-insecure-tcq(3co0z!c#fk-zfl^bpb@-3$qb389xgshueme4=$32$0+^i7
+SECRET_KEY = 'django-insecure-tcq(3co0z!c#fk-zfl^bpb@-3$qb389xgshueme4=$32$0+^i7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'HealthClinic.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-# DATABASES = {
-#   'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': 'database-1.czix73bk9dsb.us-east-1.rds.amazonaws.com',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': config('NAME'),
+#       'USER': config('USER'),
+#       'PASSWORD': config('PASSWORD'),
+#        'HOST': 'database-1.czix73bk9dsb.us-east-1.rds.amazonaws.com',
+#        'PORT': 5432,
+#   }
+# }
 
 
 # Password validation
@@ -142,4 +142,3 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
-
