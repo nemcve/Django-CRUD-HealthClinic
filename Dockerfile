38b19e284234f7c4a8cdecc749592ee77974d484
Nemanja Cvejić <37965912+nemanja023@users.noreply.github.com>
2023-03-08 18:10:26 +0100

    added dockerfile, docker-compose.yml, nginx, postgresql aws rds

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.9.16-slim-buster
+WORKDIR /djangoapp
+COPY ./HealthClinic/ ./
+RUN pip install --upgrade pip --no-cache-dir
+RUN pip install -r /djangoapp/requirements.txt --no-cache-dir
+CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
+#CMD ["python3", "/djangoapp/server.py"]
--- HealthClinic/HealthClinic/settings.py
+++ HealthClinic/HealthClinic/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tcq(3co0z!c#fk-zfl^bpb@-3$qb389xgshueme4=$32$0+^i7'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'healthclinicapp',
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -76,10 +77,22 @@ WSGI_APPLICATION = 'HealthClinic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
+# DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': 'database-1.czix73bk9dsb.us-east-1.rds.amazonaws.com',
+        'PORT': 5432,
     }
 }
 
@@ -127,3 +140,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
